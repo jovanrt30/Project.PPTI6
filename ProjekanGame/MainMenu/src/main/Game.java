088f48bde7ac2fe3e8b20f94271da958d3e6bc05
jovanrt30 <46754013+jovanrt30@users.noreply.github.com>
2019-04-04 14:37:10 +0700

    Update Game.java

--- ProjekanGame/MainMenu/src/main/Game.java
+++ ProjekanGame/MainMenu/src/main/Game.java
@@ -6,7 +6,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame window = new JFrame("Cat vs Dog");
+		JFrame window = new JFrame("SUSUTAWW");
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
